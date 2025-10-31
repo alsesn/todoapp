3738baa3d7a3d44f1c34fa9fa06dfdc977028388
Rostislav <rostislavsusak747@gmail.com>
2025-10-31 12:20:41 +0100

    Initial commit

---
+++ src/test/java/me/alsesn/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alsesn.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
