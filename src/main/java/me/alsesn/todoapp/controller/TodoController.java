41afc53bf326f3895258d79ec9399ac668877884
Rostislav <rostislavsusak747@gmail.com>
2025-10-31 17:22:18 +0100

    latest commit

--- src/main/java/me/alsesn/todoapp/controller/TodoController.java
+++ src/main/java/me/alsesn/todoapp/controller/TodoController.java
@@ -1,10 +1,11 @@
 package me.alsesn.todoapp.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.alsesn.todoapp.entity.TodoEntity;
 import me.alsesn.todoapp.repository.TodoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,5 +16,34 @@ public class TodoController {
     public String showHomePage(Model model) {
         model.addAttribute("todos", todoRepository.findAll());
         return "index";
+    }
+
+    @PostMapping("/add")
+    public String add(@RequestParam String title) {
+         TodoEntity newTodo = TodoEntity.builder()
+                .title(title)
+                .completed(false)
+                .build();
+
+         todoRepository.save(newTodo);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable Long id) {
+        TodoEntity existingTodo = todoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Todo not found: "+id));
+        existingTodo.setCompleted(true);
+        todoRepository.save(existingTodo);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        TodoEntity existingTodo = todoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Todo not found: "+id));
+        todoRepository.delete(existingTodo);
+        return "redirect:/";
     }
 }
